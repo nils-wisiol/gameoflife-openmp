0ec2887416e42d5cf1b47c34b7934750954f3525
Nils Wisiol <info@nils-wisiol.de>
2012-10-21 12:52:28 -0400

    make files

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/gameoflife-openmp-stitch.cpp 
+
+OBJS += \
+./src/gameoflife-openmp-stitch.o 
+
+CPP_DEPS += \
+./src/gameoflife-openmp-stitch.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -g -rdynamic -Wall -c -fmessage-length=0 -fopenmp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
